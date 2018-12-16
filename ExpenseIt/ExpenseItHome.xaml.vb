40024f913ab890ffb3ecaa82f5fdbd5923c5659a
Karen Gal <karengal@gmail.com>
2018-12-16 22:07:06 +0200

    wpf expenses report with xml data vb.net

---
+++ ExpenseIt/ExpenseItHome.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class ExpenseItHome
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        ' View Expense Report
+        Dim expenseReportPage As New ExpenseReportPage(Me.peopleListBox.SelectedItem)
+        Me.NavigationService.Navigate(expenseReportPage)
+    End Sub
+End Class
